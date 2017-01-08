6665d74449990d99be8b7bfb64e6c2945dc85a23
Khairul Anwar <khairul@webimp.com.sg>
2017-01-08 15:25:38 +0800

    updated: follow codeception styling

--- src/PerformanceTest.php
+++ src/PerformanceTest.php
@@ -41,31 +41,29 @@ class PerformanceTest extends \Codeception\Extension
     // we are printing test status and time taken
     public function beforeTest(\Codeception\Event\TestEvent $e)
     {
-        self::$tmpCurrentTest = \Codeception\Test\Descriptor::getTestAsString($e->getTest());
+        $this->tmpCurrentTest = \Codeception\Test\Descriptor::getTestAsString($e->getTest());
     }
 
     // we are printing test status and time taken
     public function beforeStep(\Codeception\Event\StepEvent $e)
     {
-        list($usec, $sec)       = explode(" ", microtime());
-        self::$tmpStepStartTime = (float) $sec;
+        $this->tmpStepStartTime = round((float) microtime(true), 2);
     }
 
     // we are printing test status and time taken
     public function afterStep(\Codeception\Event\StepEvent $e)
     {
-        list($usec, $sec) = explode(" ", microtime());
-        $stepEndTime      = (float) $sec;
+        $benchmark   = round((float) $this->config['benchmark'], 2);
+        $stepEndTime = round((float) microtime(true), 2);
+        $stepTime    = $stepEndTime - $this->tmpStepStartTime;
 
-        $stepTime = $stepEndTime - self::$tmpStepStartTime;
-
-        if ($stepTime >= $this->config['benchmark']) {
+        if ($stepTime > $benchmark) {
             $currentStep = (string) $e->getStep();
             $step        = new \stdClass;
             $step->name  = $currentStep;
-            $step->time  = $stepTime;
+            $step->time  = round($stepTime, 2);
 
-            self::$slowStepsByTest[self::$tmpCurrentTest][] = $step;
+            $this->slowStepsByTest[$this->tmpCurrentTest][] = $step;
         }
     }
 
@@ -75,32 +73,36 @@ class PerformanceTest extends \Codeception\Extension
         $test->name = \Codeception\Test\Descriptor::getTestAsString($e->getTest());
 
         // stack overflow: http://stackoverflow.com/questions/16825240/how-to-convert-microtime-to-hhmmssuu
-        $seconds_input = $e->getTime();
-        $seconds       = (int)($milliseconds = (int)($seconds_input * 1000)) / 1000;
-        $time          = ($seconds % 60);
+        $test->time = round((float) $e->getTime(), 2);
 
-        $test->time = $time;
-
-        self::$testTimes[] = $test;
+        $this->testTimes[] = $test;
     }
 
     // reset times and slow test arrays, in case multiple suites are launched
     public function beforeSuite(\Codeception\Event\SuiteEvent $e)
     {
-        self::$testTimes       = [];
-        self::$slowStepsByTest = [];
+        $this->testTimes       = [];
+        $this->slowStepsByTest = [];
     }
 
     public function afterSuite(\Codeception\Event\SuiteEvent $e)
     {
-        $this->writeln('<bold>' . str_pad('Slow Steps (more than ' . $this->config['benchmark'] . 's) ', $this->config['padding'], '-') . '</bold>');
+        $this->writeln('');
+        $this->writeln(str_pad('<bold>Slow Steps (more than ' . $this->config['benchmark'] . 's)</bold> ', $this->config['padding'], '-'));
 
-        foreach (self::$slowStepsByTest as $testname => $steps) {
-            $this->writeln('  ' . $testname);
+        foreach ($this->slowStepsByTest as $testname => $steps) {
+            $test_role = substr($testname, 0, stripos($testname, ':') + 1);
+            $test_name = substr($testname, stripos($testname, ':') + 1);
+
+            $testname = '<focus>' . $test_role . '</focus>' . $test_name;
+
+            $this->writeln($testname);
 
             foreach ($steps as $step) {
-                $this->writeln('<info>    ' . $step->name . ' (' . $step->time . ' s)' . '</info>');
+                $this->writeln('  ' . $step->name . ' <info>(' . $step->time . 's)</info>');
             }
         }
+
+        $this->writeln(str_pad('', $this->config['padding'], '-'));
     }
 }
